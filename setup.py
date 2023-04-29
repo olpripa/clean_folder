b835ea77fddba0a499f7f5772685a75236273d5a
Oleksandr Pripa <ol.pripa@gmail.com>
2023-04-29 18:52:18 +0200

    finally 1.1.0

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='Schone Map',
+    name='Schone_Map',
     version='1.1.0',
     description='A script for sorting files by etemsion in folder',
     author='Oleksandr Pripa',
     author_email='ol.pripa@gmail.com',
     url='https://github.com/olpripa/clean_folder',
     license='MIT',
-    classifiers = [
-    "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     packages=find_namespace_packages(),
-    entry_points = {'console_scripts': ['cleanfolder=src.cleanfolder.sort:main']}
+    entry_points={'console_scripts': [
+        'cleanfolder=src.cleanfolder.sort:main']}
 )
